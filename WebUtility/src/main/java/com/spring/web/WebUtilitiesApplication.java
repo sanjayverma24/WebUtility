20f4f0f7ac0a429bd94e434329bd9cbbe33be55b
sanjaver <sanjayasas24@gmail.com>
2024-03-23 15:30:12 +0530

    My First project

---
+++ WebUtility/src/main/java/com/spring/web/WebUtilitiesApplication.java
@@ -0,0 +1,14 @@
+package com.spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebUtilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebUtilitiesApplication.class, args);
+		System.out.println("Hello this is my first project");
+	}
+
+}
